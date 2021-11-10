89a970ec2099b40bc0035382fbd471e5468f3ae0
Mohit Yadav <yadavmohit0207@gmail.com>
2021-11-10 08:55:30 +0530

    new

---
+++ week_day.py
@@ -0,0 +1,18 @@
+a = int (input ("enter a number 1-7"))
+
+if a==1:
+    print("\n sunday")
+elif a==2:
+    print("\n monday")
+elif a==3:
+    print("\n tuesday")
+elif a==4:
+    print("\n wednesday")
+elif a==5:
+    print("\n thrusday")
+elif a==6:
+    print("\n fryday")
+elif a==7:
+    print("\n saturday")        
+else :
+    print(input ("enter a number 1-7"))
